9df2ed2d0bde37f5b6d6160d0e1b6efcbac8d791
Junho Sim <https://github.com/wnsgh2254>
2023-10-11 17:52:57 +1100

    requirements added

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding = "utf-8" ) as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
+AUTHOR_USER_NAME = "wnsgh2254"
+SRC_REPO = "cnnClassfier"
+AUTHOR_EMAIL = "wnsgh2254@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A python package for CNN app",
+    long_description=long_description,
+    Long_description_content= "text/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues"
+    },
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(where="src")
+)
+
+# conda create -n kidney python=3.8 -y
