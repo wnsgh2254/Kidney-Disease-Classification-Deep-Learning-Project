7b53cfb880a559f140985be61996a0ab1aa96c10
Junho Sim <https://github.com/wnsgh2254>
2023-10-14 15:30:54 +1100

    dvc added

--- src/cnnClassifier/pipeline/stage_04_model_evaluation_mlflow.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation_mlflow.py
@@ -15,4 +15,15 @@ class EvaluationPipeline:
         evaluation = Evaluation(eval_config)
         evaluation.evaluation()
         evaluation.save_score()
-        evaluation.log_into_mlflow()
+        # evaluation.log_into_mlflow()
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
